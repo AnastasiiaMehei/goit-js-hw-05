05aa87f0263f282bb0365dbe4f3825731b7b07a3
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-02-09 20:13:19 +0200

    4

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
 const getTotalBalanceByGender = (users, gender) => {
   const totalBalanceByGender = Object.values(users)
     .filter(user => user.gender === gender)
-    .reduce((total, balance) => total + balance, 0);
+    .reduce((total, number) => total + number.balance, 0);
   return totalBalanceByGender;
 };
 const clients = [
